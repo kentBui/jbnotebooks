de4c0e78f3fafef820402e489fef6bebd21d8430
kentFrontEndDev <duccuong.hpe@gmail.com>
2021-05-15 08:58:35 +0700

    fix bin

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,7 +1,9 @@
-#!usr/bin/env node
+#!/usr/bin/env node
 import { program } from "commander";
 import { serveCommand } from "./commands/serve";
 
 program.addCommand(serveCommand);
 
 program.parse(process.argv);
+
+// "prepublishOnly": "esbuild src/index.ts --platform=node --outfile=dist/index.js --bundle --minify --define:process.env.NODE_ENV=\\\"production\\\""
